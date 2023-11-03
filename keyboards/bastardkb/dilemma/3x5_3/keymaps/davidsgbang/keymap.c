d848b8e239cc53e6e4b0678a6a0828a8523d7478
davidsgbang <me@davidbang.email>
2023-11-03 17:27:58 -0400

    add new encoder pins

--- keyboards/bastardkb/dilemma/3x5_3/keymaps/davidsgbang/keymap.c
+++ keyboards/bastardkb/dilemma/3x5_3/keymaps/davidsgbang/keymap.c
@@ -15,7 +15,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#include QMK_KEYBOARD_H
+#include "keymap.h"
 
 enum dilemma_keymap_layers {
     LAYER_BASE = 0,
@@ -24,61 +24,29 @@ enum dilemma_keymap_layers {
     LAYER_NUM,
 };
 
-#define NAV MO(LAYER_NAV)
-#define SYM MO(LAYER_SYM)
 
-// clang-format off
+#define LAYOUT_LAYER_BASE \
+KC_Q,              KC_W,              KC_E,              KC_R,              KC_T,                    KC_Y,              KC_U,              KC_I,              KC_O,              KC_P,               \
+LGUI_T(KC_A),      LALT_T(KC_S),      LCTL_T(KC_D),      LSFT_T(KC_F),      KC_G,                    KC_H,              LSFT_T(KC_J),      LCTL_T(KC_K),      LALT_T(KC_L),      LGUI_T(KC_QUOT),    \
+KC_Z,              KC_X,              KC_C,              KC_V,              KC_B,                    KC_N,              KC_M,              KC_COMM,           KC_DOT,            KC_SLSH,\
+                                      KC_ESC,            KC_SPC,            KC_TAB,                  KC_ENT,            LT(NUM,KC_BSPC),   KC_DEL
+
+// Numerals.
+#define LAYOUT_LAYER_NUM                                                                        \
+    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    \
+    KC_LSFT, KC_LCTL, KC_LALT, KC_LGUI, KC_F11,    KC_F12,  KC_RGUI, KC_RALT, KC_RCTL, KC_RSFT, \
+    KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,     KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  \
+                      U_NA,    _______, KC_TAB,    KC_ENT,  _______, U_NA
+
+#define LAYOUT_wrapper(...) LAYOUT_split_3x5_3(__VA_ARGS__)
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  [LAYER_BASE] = LAYOUT_split_3x5_3(
-  // ╭─────────────────────────────────────────────╮ ╭─────────────────────────────────────────────╮
-          KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,       KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
-  // ├─────────────────────────────────────────────┤ ├─────────────────────────────────────────────┤
-          KC_A,    KC_S,    KC_D,    KC_F,    KC_G,       KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN,
-  // ├─────────────────────────────────────────────┤ ├─────────────────────────────────────────────┤
-          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,       KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,
-  // ╰─────────────────────────────────────────────┤ ├─────────────────────────────────────────────╯
-                            KC_A,     NAV, CW_TOGG,     KC_SPC,     SYM,    KC_A
-  //                   ╰───────────────────────────╯ ╰──────────────────────────╯
-  ),
-
-  [LAYER_NAV] = LAYOUT_split_3x5_3(
-  // ╭─────────────────────────────────────────────╮ ╭─────────────────────────────────────────────╮
-        KC_TAB, XXXXXXX, XXXXXXX, XXXXXXX, KC_VOLU,    XXXXXXX, KC_HOME,   KC_UP,  KC_END,  KC_DEL,
-  // ├─────────────────────────────────────────────┤ ├─────────────────────────────────────────────┤
-       KC_LSFT, KC_LCTL, KC_LALT, KC_RGUI, KC_VOLD,    XXXXXXX, KC_LEFT, KC_DOWN, KC_RGHT, KC_BSPC,
-  // ├─────────────────────────────────────────────┤ ├─────────────────────────────────────────────┤
-       QK_BOOT, EE_CLR,  KC_MPRV, KC_MNXT, KC_MPLY,    XXXXXXX, KC_PGDN, KC_PGUP, XXXXXXX,  KC_ENT,
-  // ╰─────────────────────────────────────────────┤ ├─────────────────────────────────────────────╯
-                            KC_A, _______, KC_LSFT,     KC_SPC, _______,    KC_A
-  //                   ╰───────────────────────────╯ ╰──────────────────────────╯
-  ),
-
-  [LAYER_SYM] = LAYOUT_split_3x5_3(
-  // ╭─────────────────────────────────────────────╮ ╭─────────────────────────────────────────────╮
-        KC_ESC, KC_LBRC, KC_LCBR, KC_LPRN, KC_TILD,    KC_CIRC, KC_RPRN, KC_RCBR, KC_RBRC,  KC_GRV,
-  // ├─────────────────────────────────────────────┤ ├─────────────────────────────────────────────┤
-       KC_MINS, KC_ASTR,  KC_EQL, KC_UNDS,  KC_DLR,    KC_HASH, KC_RGUI, KC_RALT, KC_RCTL, KC_RSFT,
-  // ├─────────────────────────────────────────────┤ ├─────────────────────────────────────────────┤
-       KC_PLUS, KC_PIPE,   KC_AT, KC_SLSH, KC_PERC,    _______, KC_BSLS, KC_AMPR, KC_QUES, KC_EXLM,
-  // ╰─────────────────────────────────────────────┤ ├─────────────────────────────────────────────╯
-                            KC_A, _______, KC_LSFT,     KC_SPC, _______,    KC_A
-  //                   ╰───────────────────────────╯ ╰──────────────────────────╯
-  ),
-
-  [LAYER_NUM] = LAYOUT_split_3x5_3(
-  // ╭─────────────────────────────────────────────╮ ╭─────────────────────────────────────────────╮
-          KC_1,    KC_2,    KC_3,    KC_4,    KC_5,       KC_6,    KC_7,    KC_8,    KC_9,   KC_0,
-  // ├─────────────────────────────────────────────┤ ├─────────────────────────────────────────────┤
-       KC_LSFT, KC_LCTL, KC_LALT, KC_LGUI,  KC_F11,     KC_F12, KC_RGUI, KC_RALT, KC_RCTL, KC_RSFT,
-  // ├─────────────────────────────────────────────┤ ├─────────────────────────────────────────────┤
-         KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,      KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,
-  // ╰─────────────────────────────────────────────┤ ├─────────────────────────────────────────────╯
-                            KC_A, _______, KC_LSFT,     KC_SPC, _______,    KC_A
-  //                   ╰───────────────────────────╯ ╰──────────────────────────╯
-  ),
+  [LAYER_BASE] = LAYOUT_wrapper(LAYOUT_LAYER_BASE),
+  //[LAYER_MBO] = LAYOUT_wrapper(LAYOUT_LAYER_MBO),
+  //[LAYER_MEDIA] = LAYOUT_wrapper(LAYOUT_LAYER_MEDIA),
+  //[LAYER_NAV] = LAYOUT_wrapper(LAYOUT_LAYER_NAV),
+  //[LAYER_MOUSE] = LAYOUT_wrapper(LAYOUT_LAYER_MOUSE),
+  //[LAYER_SYM] = LAYOUT_wrapper(LAYOUT_LAYER_SYM),
+  [LAYER_NUM] = LAYOUT_wrapper(LAYOUT_LAYER_NUM),
+  //[LAYER_FUN] = LAYOUT_wrapper(LAYOUT_LAYER_FUN),
 };
-// clang-format on
-
-layer_state_t layer_state_set_user(layer_state_t state) {
-    return update_tri_layer_state(state, LAYER_NAV, LAYER_SYM, LAYER_NUM);
-}
