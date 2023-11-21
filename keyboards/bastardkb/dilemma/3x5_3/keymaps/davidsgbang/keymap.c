ac603cec131a352a3a8df6074936c4689f03be39
davidsgbang <me@davidbang.email>
2023-11-21 18:05:52 -0500

    wip

--- keyboards/bastardkb/dilemma/3x5_3/keymaps/davidsgbang/keymap.c
+++ keyboards/bastardkb/dilemma/3x5_3/keymaps/davidsgbang/keymap.c
@@ -27,30 +27,30 @@ enum dilemma_keymap_layers {
 
 #define LAYOUT_LAYER_BASE \
 KC_Q,              KC_W,              KC_E,              KC_R,                  KC_T,                    KC_Y,                  KC_U,                    KC_I,              KC_O,              KC_P,               \
-LSFT_T(KC_A),      LCTL_T(KC_S),      LGUI_T(KC_D),      LALT_T(KC_F),          KC_G,                    KC_H,                  LALT_T(KC_J),            LGUI_T(KC_K),      LCTL_T(KC_L),      LSFT_T(KC_SCLN),    \
-KC_Z,              KC_X,              KC_C,              KC_V,                  KC_B,                    KC_N,                  KC_M,                    KC_COMM,           KC_DOT,            KC_SLSH,\
+LSFT_T(KC_A),      KC_S,              KC_D,              KC_F,                  KC_G,                    KC_H,                  KC_J,                    KC_K,              KC_L,              RSFT_T(KC_SCLN),    \
+LCTL_T(KC_Z),      LGUI_T(KC_X),      LALT_T(KC_C),      KC_V,                  KC_B,                    KC_N,                  KC_M,                    RALT_T(KC_COMM),   RGUI_T(KC_DOT),    RCTL_T(KC_SLSH),\
                                       KC_BTN1,           KC_TAB,                LT(SYM, KC_SPC),         LT(NUM,KC_ENT),        KC_BSPC,                 KC_BTN2
 
 // Numerals.
 #define LAYOUT_LAYER_NUM                                                                        \
 KC_1,           KC_2,           KC_3,           KC_4,           KC_5,           KC_6,           KC_7,           KC_8,           KC_9,           KC_0,    \
-KC_LSFT,        KC_LCTL,        KC_LGUI,        KC_LALT,        CW_TOGG,        KC_QUOT,        KC_MINS,        KC_EQL,         KC_LBRC,        KC_RBRC, \
-U_NA,           U_NA,           U_NA,           U_NA,           U_NA,           U_NA,           KC_BSLS,        U_NA,           U_NA,           U_NA,  \
+KC_LSFT,        U_NA,           U_NA,           U_NA,           CW_TOGG,        KC_QUOT,        KC_MINS,        KC_EQL,         KC_LBRC,        KC_RBRC, \
+KC_LCTL,        KC_LGUI,        KC_LALT,        U_NA,           U_NA,           U_NA,           KC_BSLS,        U_NA,           U_NA,           U_NA,  \
                                 U_NA,           KC_ESC,         MO(NAV),        _______,        KC_DEL,         U_NA
 
 // SYM.
 #define LAYOUT_LAYER_SYM                                                                        \
 KC_EXLM,        KC_AT,          KC_HASH,        KC_DLR,         KC_PERC,        KC_CIRC,        KC_AMPR,        KC_ASTR,        KC_LPRN,        KC_RPRN,    \
-KC_LSFT,        KC_LCTL,        KC_LGUI,        KC_LALT,        CW_TOGG,        KC_DQT,         KC_UNDS,        KC_PLUS,        KC_LCBR,        KC_RCBR, \
-U_NA,           U_NA,           U_NA,           U_NA,           U_NA,           U_NA,           KC_PIPE,        KC_LT,          KC_GT,          KC_QUES,  \
+KC_LSFT,        U_NA,           U_NA,           U_NA,           CW_TOGG,        KC_DQT,         KC_UNDS,        KC_PLUS,        KC_LCBR,        KC_RCBR, \
+KC_LCTL,        KC_LGUI,        KC_LALT,        U_NA,           U_NA,           U_NA,           KC_PIPE,        KC_LT,          KC_GT,          KC_QUES,  \
                                 U_NA,           KC_ESC,         _______,        MO(NAV),        KC_DEL,         U_NA
 
 // NAV.
 #define LAYOUT_LAYER_NAV                                                                        \
 KC_F1,          KC_F2,          KC_F3,          KC_F4,          KC_F5,          KC_F6,          KC_F7,          KC_F8,          KC_F9,          KC_F10,    \
-KC_LSFT,        KC_LCTL,        KC_LGUI,        KC_LALT,        KC_CAPS,        U_NA,           KC_LEFT,        KC_DOWN,        KC_UP,          KC_RIGHT, \
-U_NA,           U_NA,           U_NA,           U_NA,           U_NA,           U_NA,           U_NA,           U_NA,           U_NA,           U_NA,  \
-                                U_NA,           _______,        _______,        _______,        _______,        U_NA
+KC_LSFT,        U_NA,           U_NA,           U_NA,           KC_CAPS,        U_NA,           KC_LEFT,        KC_DOWN,        KC_UP,          KC_RIGHT, \
+KC_LCTL,        KC_LGUI,        KC_LALT,        U_NA,           U_NA,           U_NA,           U_NA,           U_NA,           U_NA,           U_NA,  \
+                                BL_TOGG,        _______,        _______,        _______,        _______,        BL_TOGG
 
 
 #define LAYOUT_wrapper(...) LAYOUT_split_3x5_3(__VA_ARGS__)
